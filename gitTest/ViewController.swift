46740e8d8711c8d258b6e5fe8e207642331c5a0b
ruwan9 <ruwan9@kakao.com>
2022-03-12 23:19:35 +0900

    Initial Commit

---
+++ gitTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gitTest
+//
+//  Created by Wansoo Ryu on 2022/03/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
